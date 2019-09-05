30c1f0e7c7a371a8e2ccaa10327520e8c4e6c5b4
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-09-05 08:27:44 +0000

    Automatically backed up by Learn

---
+++ learning_comments.rb
@@ -0,0 +1,15 @@
+#run_code_inside = true
+#puts "code before if...end"
+#if run_code_inside
+ # puts "code inside"
+#end
+#puts "Code after if...end"
+
+chance_of_rain = 0.2
+puts "let's go  outside!"
+if chance_of_rain > 0.5
+  puts "Pack an umbrella!"
+else
+  puts "Enjoy the fine day!"
+end
+puts "oh, and always wear sunscreen!"
